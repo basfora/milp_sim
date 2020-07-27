4f010d87bb0bee51b1fc438c85a82081c44d16c9
basfora <beatriz.asfora@gmail.com>
2020-07-27 19:25:30 -0400

    mysearcher2

--- milp_risk/scripts/r_classes.py
+++ milp_risk/scripts/r_classes.py
@@ -1,4 +1,5 @@
 from milp_mespp.classes.searcher import MySearcher
+from milp_mespp.classes.inputs import MyInputs
 from milp_mespp.core import extract_info as ext
 
 
@@ -7,14 +8,14 @@ class MySearcher2(MySearcher):
     """Child searcher class for milp_sim (risk)
     Parent class: MySearcher from milp_mespp.classes"""
 
-    def __init__(self, searcher_id: int, v0: int, g, kappa: int or list, rho: float, risk_perception='d',
-                 capture_range=0, zeta=None, my_seed=None):
+    def __init__(self, searcher_id: int, v0: int, g, kappa: int or list, rho: float, danger_perception='point',
+                 prob_default=0.5, capture_range=0, zeta=None, my_seed=None):
         # note: init overrides parent class init
 
         # inherit parent class methods and properties
         super().__init__(searcher_id, v0, g, capture_range, zeta, my_seed)
 
-        self.kappa_levels = [1, 2, 3, 4, 5]
+        self.danger_levels = [1, 2, 3, 4, 5]
 
         # initial thresholds
         # danger threshold
@@ -28,9 +29,9 @@ class MySearcher2(MySearcher):
         self.stored_rho[0] = self.rho_0
 
         # type of danger threshold (d - deterministic or p - probabilistic)
-        self.danger_perception = risk_perception
+        self.danger_perception = danger_perception
         # start danger thresholds
-        self.init_kappa()
+        self.init_kappa(prob_default)
 
         # current (updated throughout sim)
         self.kappa = kappa
@@ -44,30 +45,29 @@ class MySearcher2(MySearcher):
     def adjust_rho(self, b_c: float):
         """Adjust target belief threshold based on current belief of capture
         b_c = 0, rho = rho_0
-        b_c = x, rho  = """
-        # TODO finish this
+        b_c = x, rho  = rho_0 x b_c"""
 
-    def init_kappa(self):
+        new_rho = self.rho_0 * (1 - b_c)
+        self.set_new_rho(new_rho)
+        self.store_rho(new_rho)
+
+        return new_rho
+
+    def init_kappa(self, prob_default=0.5):
         """Initialize the danger thresholds according to type"""
 
-        prob_default = 0.5
         my_kappa = self.kappa_0
 
-        # TODO finish this (switch order danger_perception > isinstance)
-
-        # input was an integer
         if isinstance(self.kappa_0, int):
-
-            # check if deterministic or probabilistic
-            if self.danger_perception == 'p':
-                my_kappa = []
-                # adjust using default prob
-                for level in self.kappa_levels:
-                    if level < self.kappa_0:
-                        my_kappa.append(1)
-                    else:
+            my_kappa = []
+            for level in self.danger_levels:
+                if level <= self.kappa_0:
+                    my_kappa.append(1.0)
+                else:
+                    if self.danger_perception == 'pdf':
                         my_kappa.append(prob_default)
-        #elif isinstance(self.kappa_0, list):
+                    else:
+                        my_kappa.append(0.0)
 
         self.stored_kappa[0] = my_kappa
 
@@ -95,4 +95,14 @@ class MySearcher2(MySearcher):
         self.store_rho(new_rho)
 
 
+class MyInputs2(MyInputs):
+    """Child inputs class for milp_sim (risk)
+    Parent class: MyInputs from milp_mespp.classes"""
+
+    def __init__(self):
+        # inherit parent class methods and properties
+        super().__init__()
+
+        self.rho = None
+        self.kappa = None
 
